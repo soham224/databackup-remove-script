96e460e3eb7b4711421ae0fcc599024215d32b2e
Shruti Agarwal <shruti.tuskerai@gmail.com>
2025-09-23 12:03:17 +0530

    Code changes Delete Script

---
+++ delete.py
@@ -0,0 +1,31 @@
+import pymongo
+
+DATABASE_NAME = "auto-serving-results"
+COLLECTION_NAME = "results"
+HOST = "localhost"
+PORT = 27017
+
+year = 2025
+month = 6
+user_id = 97
+
+client = pymongo.MongoClient(host=HOST, port=PORT)
+db = client[DATABASE_NAME]
+collection = db[COLLECTION_NAME]
+
+
+delete_condition = {
+    "user_id": str(user_id),
+    "$expr": {
+        "$and": [
+            {"$eq": [{"$year": "$created_date"}, year]},
+            {"$eq": [{"$month": "$created_date"}, month]},
+        ]
+    },
+}
+# print(delete_condition)
+result = collection.delete_many(delete_condition)
+# print(result)
+print("FINAL OUTPUT :: :: :: :: ")
+print(result.deleted_count)
+
